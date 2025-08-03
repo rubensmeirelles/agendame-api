d31e71c2ca0406b1bdf9dc34cf38a5846e73c3df
Rubens <rubens.meirelles@postalsaude.com.br>
2025-08-03 19:58:30 -0300

    Endpoint de login

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::post('login', LoginController::class);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/teste-cors', function () {
+    return response()->json(['ok' => true]);
 });
